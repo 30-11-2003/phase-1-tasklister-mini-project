29e4ebaf128cff3cf91de19e48019f6569364901
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2023-10-13 21:33:52 +0300

    Updated js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form= document.querySelector('form').addEventListener('submit',e)=
+    e.preventDefault()
+    console.log(e.target.new_description.value)
+  
+  }
+);
+
+
+function buildToDo(todo){
+  let p= document.createElement("p");
+  let btn = document.createElement ("button");
+  btn.addEventListener('click',(handleDelete)=>{
+    p.textContent= todo
+    btn.textContent='x'
+    document.querySelector('#create-task-form').appendChild(p)
+
+  let dropdown =document.createElement("select")
+dropdownValues.forEach((value, index) => {
+  const option = document.createElement('option');
+  option.value = `high priority${index + 1}`; // Assign a unique value to each option
+  option.value = `medium${index + 1}`;
+  option.value = `low${index + 1}`;
+  option.text = value;
+  dropdown.appendChild(option);
+
+  dropdown.value[0].style.color="green";
+  dropdown.value[1].style.color="yellow";
+  dropdown.value[2].style.color="red";
+  dropdown.sort()
 });
+
+
+
+function handleDelete (e){
+  e.target.parentNode.remove()
+  
+}
+
+
+
+document.createElement('form').addEventListener(submit, ()=>
+form.textContent="user"
+
+   
+  )
